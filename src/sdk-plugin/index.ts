d6c2b96204ad5f3780abf10934a22b002084bf12
MaitriGurey1 <maitri.gurey@contentstack.com>
2023-05-15 10:17:38 +0530

    no changes

--- src/sdk-plugin/index.ts
+++ src/sdk-plugin/index.ts
@@ -9,8 +9,8 @@ if (Object.keys(Env).length > 0) {
     api_key: Env.CONTENTSTACK_API_KEY,
     delivery_token: Env.CONTENTSTACK_DELIVERY_TOKEN,
     environment: Env.CONTENTSTACK_ENVIRONMENT,
-    branch: Env.CONTENTSTACK_BRANCH,
-    region: Env.CONTENTSTACK_REGION,
+    branch: process.env.CONTENTSTACK_BRANCH,
+    region: process.env.CONTENTSTACK_REGION,
     live_preview: {
       host: Env.CONTENTSTACK_API_HOST,
       enable: Env.CONTENTSTACK_LIVE_PREVIEW === 'true',
